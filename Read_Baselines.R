54ef93fc16487cdcff95b435bdaecc6b68c58cdd
ciaran-dk <ciaran@murraynet.dk>
2019-11-15 10:52:59 +0100

    species name splitting now done in a function

--- Read_Baselines.R
+++ Read_Baselines.R
@@ -5,8 +5,6 @@ rm(list=ls())
 
 folder <- "../7-Data - 190183 NISAR/nst/"
 
-# ---------- Read Appendix 1 -----------------
-df1<-read_excel(paste0(folder,"GES 22-2019-06 D2 Baselines Appendix 1.xlsx"),sheet = "all NIS")
 
 # split species name from author names, depending on postions of spaces and brackets
 # some special cases - we caqnt distinguish between a third species name component and the author:
@@ -21,43 +19,60 @@ Species<- c("Amphistegina cf. papillosa",
             "Diopatra hupferiana hupferiana",
             "Diopatra hupferiana monroi")
 
-dfSpecies<-data.frame(Species,stringsAsFactors=F)
-dfSpecies <- dfSpecies %>%
-  mutate(n=nchar(Species))
-
-names(df1)[1]<-"Species"
-df1 <- df1 %>%
-  mutate(Species = gsub(intToUtf8(160)," ",Species)) %>%
-  mutate(ns=str_locate_all(Species," ")) %>%
-  mutate(nb=str_locate_all(Species,"\\(")) %>%
-  mutate(nvar=str_locate_all(Species,"var\\."),
-         nsub=str_locate_all(Species,"subsp\\.")) %>%
-  mutate(nvar=sapply(nvar,function(x) unlist(x)[1]),
-         nsub=sapply(nsub,function(x) unlist(x)[1])) %>%
-  mutate(ns2=sapply(ns,function(x) unlist(x)[2]),
-         ns3=sapply(ns,function(x) unlist(x)[3]),
-         ns4=sapply(ns,function(x) unlist(x)[4]),
-         nb1=sapply(nb,function(x) unlist(x)[1])) %>%
-  mutate(nsplit=ifelse(is.na(nsub) & is.na(nvar),
-                       ifelse(is.na(nb1),ns2,ifelse(nb1<ns2,ns3,ns2)),
-                       ns4)-1,
-         n=NA)
-
+SplitSpeciesName<-function(df,Species=NULL){
+  
+  # Species list - Species names which don't fit the other rules
+  if(!is.null(Species)){
+    dfSpecies<-data.frame(Species,stringsAsFactors=F)
+    dfSpecies <- dfSpecies %>%
+      mutate(n=nchar(Species))
+  }else{
+    dfSpecies<-data.frame()
+  }
+  
+  df<-df %>%
+    mutate(ns=str_locate_all(Species," ")) %>%
+    mutate(nb=str_locate_all(Species,"\\(")) %>%
+    mutate(nvar=str_locate_all(Species,"var\\."),
+           nsub=str_locate_all(Species,"subsp\\.")) %>%
+    mutate(nvar=sapply(nvar,function(x) unlist(x)[1]),
+           nsub=sapply(nsub,function(x) unlist(x)[1])) %>%
+    mutate(ns2=sapply(ns,function(x) unlist(x)[2]),
+           ns3=sapply(ns,function(x) unlist(x)[3]),
+           ns4=sapply(ns,function(x) unlist(x)[4]),
+           nb1=sapply(nb,function(x) unlist(x)[1])) %>%
+    mutate(nsplit=ifelse(is.na(nsub) & is.na(nvar),
+                         ifelse(is.na(nb1),ns2,ifelse(nb1<ns2,ns3,ns2)),
+                         ns4)-1,
+           n=NA)
+  
   for(i in 1:nrow(dfSpecies)){
     sx <- dfSpecies[i,"Species"]
     nx <- dfSpecies[i,"n"]
-    df1 <- df1 %>%
+    df <- df %>%
       mutate(n=ifelse(is.na(n),
                       ifelse(substr(Species,1,nx)==sx,nx,NA),
                       n))
   }
-
-  df1 <- df1 %>%
+  
+  df <- df %>%
     mutate(nsplit=ifelse(is.na(n),nsplit,n)) %>%
     mutate(Species2=substr(Species,1,nsplit)) 
   
-  df1 <- df1 %>%
-  select(-c(n,ns,nb,nvar,nsub,ns2,ns3,ns4,nb1,nsplit))
+  df <- df %>%
+    select(-c(n,ns,nb,nvar,nsub,ns2,ns3,ns4,nb1,nsplit))  
+  return(df)
+}
+
+
+# ---------- Read Appendix 1 -----------------
+df1<-read_excel(paste0(folder,"GES 22-2019-06 D2 Baselines Appendix 1.xlsx"),sheet = "all NIS")
+
+names(df1)[1]<-"Species"
+df1 <- df1 %>%
+  mutate(Species = gsub(intToUtf8(160)," ",Species)) 
+
+df1<-SplitSpeciesName(df1,Species)
 
 df1 <- df1 %>%
   group_by(Species,Species2) %>%
@@ -68,8 +83,6 @@ df1s <- df1
 df1 <- df1  %>%
   ungroup() %>%
   select(SpeciesOriginal=Species,Species=Species2,-n)
-
-
 
 # ---------- Read Appendix 2 -----------------
 rm("df2")
@@ -121,38 +134,9 @@ df2 <- df2 %>%
   summarise(Status=paste0(Status, collapse="/")) %>%
   ungroup()
 
-
+df2 <- SplitSpeciesName(df2,Species)
+  
 df2 <- df2 %>%
-  mutate(ns=str_locate_all(Species," ")) %>%
-  mutate(nb=str_locate_all(Species,"\\(")) %>%
-  mutate(nvar=str_locate_all(Species,"var\\."),
-         nsub=str_locate_all(Species,"subsp\\.")) %>%
-  mutate(nvar=sapply(nvar,function(x) unlist(x)[1]),
-         nsub=sapply(nsub,function(x) unlist(x)[1])) %>%
-  mutate(ns2=sapply(ns,function(x) unlist(x)[2]),
-         ns3=sapply(ns,function(x) unlist(x)[3]),
-         ns4=sapply(ns,function(x) unlist(x)[4]),
-         nb1=sapply(nb,function(x) unlist(x)[1])) %>%
-  mutate(nsplit=ifelse(is.na(nsub) & is.na(nvar),
-                       ifelse(is.na(nb1),ns2,ifelse(nb1<ns2,ns3,ns2)),
-                       ns4)-1,
-         n=NA) 
-
-for(i in 1:nrow(dfSpecies)){
-  sx <- dfSpecies[i,"Species"]
-  nx <- dfSpecies[i,"n"]
-  df2 <- df2 %>%
-    mutate(n=ifelse(is.na(n),
-                    ifelse(substr(Species,1,nx)==sx,nx,NA),
-                    n))
-}
-
-df2 <- df2 %>%
-  mutate(nsplit=ifelse(is.na(n),nsplit,n)) %>%
-  mutate(Species2=substr(Species,1,nsplit)) 
-
-df2 <- df2 %>%
-  select(-c(n,ns,nb,nvar,nsub,ns2,ns3,ns4,nb1,nsplit)) %>%
   select(SpeciesOriginal=Species,Species=Species2,Status) %>%
   left_join(df2spr,by=c("SpeciesOriginal"="Species"))
 
