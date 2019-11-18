6e00863265fc97e7ff1349b08fbc8c24be5b9191
ciaran-dk <ciaran@murraynet.dk>
2019-11-18 14:52:09 +0100

    getting lat,lon from marineregions.org

--- distributions_analysis.R
+++ distributions_analysis.R
@@ -3,6 +3,23 @@ library(tidyverse)
 library(jsonlite)
 library(httr)
 
+# Function mrginfo(MRGID)
+# to get lat,lon extent information from marineregions.org 
+
+mrginfo<-function(MRGID){
+  url<-sprintf("http://marineregions.org/rest/getGazetteerRecordByMRGID.json/%d/",MRGID)
+  df <- data.frame()
+  
+  x<-http_status(GET(url))
+  if(x$reason!="OK"){
+    cat(paste0(MRGID,": ",x$reason,"\n"))
+    return(df)
+    }
+  df <- fromJSON(url)
+  return(df)
+  }
+
+
 df <- read.table("output/ODA_Species_Distributions.csv",sep=";",header=T,stringsAsFactors=F)
 df <- df %>% 
   filter(locality!="")
@@ -17,41 +34,24 @@ df <- df %>%
   left_join(df1,by=c("locality","locationID"))
 
 
-mrginfo<-function(MRGID){
-  url<-sprintf("http://marineregions.org/rest/getGazetteerWMSes.json/%d/",MRGID)
-  
-  wms <- data.frame()
-  
-  x<-http_status(GET(url))
-  if(x$reason!="OK"){
-    cat(paste0(MRGID,": ",x$reason,"\n"))
-    return(wms)
-    }
-  wms <- fromJSON(url)
-  #wms<-list(wms$MRGID,wms$url,wms$namespace,wms$featureType,wms$featureName)
-  return(wms)
-  }
-
 df <- df %>%
   mutate(MRGID=as.numeric(substr(locationID,32,nchar(locationID))))
 
 #df <- df[1:9,]
-df <- df[c(32,462,868,892),]
-
+#df <- df[c(32,462,868,892),]
 
 df <- df %>%
   mutate(MRGinfo=lapply(MRGID, function(x) mrginfo(x)))
 
 df <- df %>%
-  mutate(url=sapply(MRGinfo, function(x) unlist(x)[2]),
-         namespace=sapply(MRGinfo, function(x) unlist(x)[3]),
-         featureType=sapply(MRGinfo, function(x) unlist(x)[4]),
-         featureName=sapply(MRGinfo, function(x) unlist(x)[5]),
-         MRGID2=sapply(MRGinfo, function(x) unlist(x)[1])
+  mutate(placeType=sapply(MRGinfo, function(x) unlist(x)["placeType"]),
+         latitude=sapply(MRGinfo, function(x) unlist(x)["latitude"]),
+         longitude=sapply(MRGinfo, function(x) unlist(x)["longitude"]),
+         minLatitude=sapply(MRGinfo, function(x) unlist(x)["minLatitude"]),
+         maxLongitude=sapply(MRGinfo, function(x) unlist(x)["maxLongitude"]),
+         minLatitude=sapply(MRGinfo, function(x) unlist(x)["minLatitude"]),
+         maxLatitude=sapply(MRGinfo, function(x) unlist(x)["maxLatitude"])
          )
 
+write.table(dfResults,file="output/ODA_Species_Distributions_LatLon.csv",col.names=T,row.names=F,sep=";",na="")
 
-url<-"http://geo.vliz.be/geoserver/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
-x<-http_status(GET(url))
-dfx <- fromJSON(url)
-#url<- "http://www.marineregions.org/downloads.php#ihoeez" 
